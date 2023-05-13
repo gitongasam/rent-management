a64cc4b12f0aa678a3fcf6c34ffe813a651ac997
Developer sam <gitongas684@gmail.com>
2023-05-13 17:00:35 +0300

    house service

---
+++ servce-registry/src/test/java/com/devSam/servceregistry/ServceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSam.servceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
